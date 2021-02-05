d5c855cd75f330a610768bda0266538f5a2178b2
DESKTOP-R627CBL\elbrown <elbrown@kakao.com>
2021-02-05 23:53:56 +0900

    버그수정, windows/Linux 텍스트 인코딩 선택 추가

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ class MyApp(QWidget):
         self.pwidth = QLineEdit()
         self.pheight = QLineEdit()
 
+        # 확장자 체크 버튼
+        self.btnwin = QRadioButton('Windows(cp949,euc-kr)', self)
+        self.btnlin = QRadioButton('Unix/Linux(utf-8)', self)
+
         self.xllocation.setMaximumWidth(395)
         self.dirlocation.setMaximumWidth(395)
         self.prow.setMaximumWidth(75)
@@ -35,6 +39,12 @@ class MyApp(QWidget):
         #초기 값 지정(config.ini에서 가져옴)
         self.config = configparser.ConfigParser()
         self.config.read('./config.ini')
+
+        if self.config['Settings']['encoding'] == 'True':
+            self.btnwin.setChecked(True)
+        else:
+            self.btnlin.setChecked(True)
+
         self.pcol.setText(self.config['Settings']['column'])
         self.prow.setText(self.config['Settings']['row'])
         self.pwidth.setText(self.config['Settings']['width'])
@@ -46,11 +56,10 @@ class MyApp(QWidget):
         QMessageBox.information(self, "주의사항",
                                 '1. 작업할 엑셀 파일을 반드시 백업해주세요.\r\n' \
                                 '2. 엑셀파일이 실행중인 경우 오류가 발생합니다.\r\n' \
-                                '3. 입력된 파싱 패턴을 찾지 못하면 내용이 저장되지 않습니다.\r\n'\
-                                '4. UTF-8 타입의 텍스트 인코딩만 지원됩니다.\r\n')
+                                '3. 입력된 파싱 패턴을 찾지 못하면 내용이 저장되지 않습니다.')
 
     def initUI(self):
-        self.setWindowTitle('InfraProject v1.0 by.Elbrown')
+        self.setWindowTitle('InfraProject v2.0 by.Elbrown')
 
         #GridLayOut
         self.setLayout(self.grid)
@@ -60,24 +69,29 @@ class MyApp(QWidget):
         self.grid.addWidget(xlbtn, 0, 0)
         self.grid.addWidget(dirbtn, 1, 0)
 
+        # 인코딩 선택 버튼 추가
+        self.grid.addWidget(QLabel('텍스트 파일 인코딩 선택'), 2, 0)
+        self.grid.addWidget(self.btnwin, 2, 1)
+        self.grid.addWidget(self.btnlin, 2, 2)
+
         # Label
-        self.grid.addWidget(QLabel('파싱 시작 문자열 입력'), 2, 0)
-        self.grid.addWidget(QLabel('파싱 종료 문자열 입력'), 3, 0)
-        self.grid.addWidget(QLabel('셀 위치 열/행'), 2, 2)
-        self.grid.addWidget(QLabel('셀 너비/높이'), 3, 2)
+        self.grid.addWidget(QLabel('파싱 시작 문자열 입력'), 3, 0)
+        self.grid.addWidget(QLabel('파싱 종료 문자열 입력'), 4, 0)
+        self.grid.addWidget(QLabel('셀 위치 열/행'), 3, 2)
+        self.grid.addWidget(QLabel('셀 너비/높이'), 4, 2)
 
         #Edit버튼 붙이기
         self.grid.addWidget(self.xllocation, 0, 1, 1, 4)
         self.grid.addWidget(self.dirlocation, 1, 1, 1, 4)
 
-        self.grid.addWidget(self.pstart, 2, 1)
-        self.grid.addWidget(self.pend, 3, 1)
+        self.grid.addWidget(self.pstart, 3, 1)
+        self.grid.addWidget(self.pend, 4, 1)
 
-        self.grid.addWidget(self.pcol, 2, 3)
-        self.grid.addWidget(self.prow, 2, 4)
+        self.grid.addWidget(self.pcol, 3, 3)
+        self.grid.addWidget(self.prow, 3, 4)
 
-        self.grid.addWidget(self.pwidth, 3, 3)
-        self.grid.addWidget(self.pheight, 3, 4)
+        self.grid.addWidget(self.pwidth, 4, 3)
+        self.grid.addWidget(self.pheight, 4, 4)
 
         #Button
         scbtn = QPushButton('설정저장', self)
@@ -85,10 +99,10 @@ class MyApp(QWidget):
         oebtn = QPushButton('엑셀열기', self)
         closebtn = QPushButton('종료하기', self)
 
-        self.grid.addWidget(scbtn, 4, 0)
-        self.grid.addWidget(psbtn, 4, 1, 1, 2)
-        self.grid.addWidget(oebtn, 4, 3)
-        self.grid.addWidget(closebtn, 4, 4)
+        self.grid.addWidget(scbtn, 5, 0)
+        self.grid.addWidget(psbtn, 5, 1, 1, 2)
+        self.grid.addWidget(oebtn, 5, 3)
+        self.grid.addWidget(closebtn, 5, 4)
 
         #이벤트 연결
         scbtn.clicked.connect(self.saveConfig)
@@ -111,6 +125,7 @@ class MyApp(QWidget):
 
     def saveConfig(self):
         #현재 지정된 설정으로 옵션 변경
+        self.config.set("Settings", "encoding", str(self.btnwin.isChecked()))
         self.config.set("Settings", "column", self.pcol.text())
         self.config.set("Settings", "row", self.prow.text())
         self.config.set("Settings", "width", self.pwidth.text())
@@ -125,21 +140,21 @@ class MyApp(QWidget):
     def parsing(self):
         program=parsing.MyParsing()
         program.getTxt(self.dirlocation.text())
-        program.initVal(self.pstart.text(),self.pend.text(), self.pcol.text(), self.prow.text(), self.pwidth.text(), self.pheight.text())
+        program.initVal(str(self.btnwin.isChecked()), self.pstart.text(),self.pend.text(), self.pcol.text(), self.prow.text(), self.pwidth.text(), self.pheight.text())
         if program.checkSheet(self.xllocation.text()):
             QMessageBox.critical(self, "메세지", '실행된 엑셀 파일을 종료해주세요.')
             return
-        # Return 값에 따른 에러처리 1:텍스트파일에러, 2:파싱문자열에러, 3:인코딩에러
+        # Return 값에 따른 에러처리 1:텍스트파일에러, 2:파싱문자열에러, 3:잘못된파싱, 4:인코딩에러
         val=program.writeCell(self.xllocation.text())
         if val==1:
             QMessageBox.warning(self, "에러메세지", '텍스트 파일 오픈 에러, 파일이 존재하는지 확인해주세요.')
-            return
         elif val==2:
             QMessageBox.warning(self, "에러메세지", '시작/끝 파싱포인트가 매칭되지 않았습니다. 문자열 또는 정규표현식을 다시 확인해주세요.')
-            return
         elif val==3:
-            QMessageBox.warning(self, "에러메세지", 'UTF-8 텍스트 파일의 인코딩만 지원합니다.')
-            return
+            QMessageBox.warning(self, "에러메세지", '한셀에 32700문자를 넘어간 경우 에러가 발생합니다.\r\n' \
+                                                  '(대게 의도치 않게 파싱이 잘못된 경우 발생)')
+        elif val==4:
+            QMessageBox.warning(self, "에러메세지", '텍스트 파일의 인코딩(Windows/Linux) 체크를 확인해주세요.')
         else:
             QMessageBox.information(self, "메세지", '작업이 완료되었습니다. 엑셀열기를 클릭해주세요.')
 
--- parsing.py
+++ parsing.py
@@ -7,15 +7,16 @@ import re
 class MyParsing:
     def __init__(self):
         super().__init__()
-        self.txtList = []  # 텍스트 배열
-        self.wsList = []  # 워크시트명 배열
+        self.txtList = []
+        self.wsList = []
         self.start = ""
         self.end = ""
         self.col = 0
         self.row = 0
         self.dir = ""
 
-    def initVal(self, start, end, col, row, width, height):
+    def initVal(self, encode, start, end, col, row, width, height):
+        self.isWindows = encode
         self.start = start
         self.end = end
         self.col = col
@@ -24,7 +25,7 @@ class MyParsing:
         self.height = height
 
     # FH는 파일핸들러
-    # Return 값에 따른 에러처리 1:텍스트파일에러, 2:파싱문자열에러, 3:인코딩에러
+    # Return 값에 따른 에러처리 1:텍스트파일에러, 2:파싱문자열에러, 3:잘못된파싱, 4:인코딩에러
     def writeCell(self, filename):
         stcol = self.col
         strow = self.row
@@ -33,26 +34,32 @@ class MyParsing:
         FH = None
         # 워크시트 별로 작업 진행
         for wsname in self.txtList:
+            selected_encode = ''
+            if self.isWindows == 'True':
+                selected_encode = 'cp949'
+            else:
+                selected_encode = 'utf-8'
+            # 텍스트 파일을 연다. UTF-8로 인코딩 된 텍스트 파일만 불러올 수 있다.
             try:
-                # 텍스트 파일을 연다. UTF-8로 인코딩 된 텍스트 파일만 불러올 수 있다.
-                FH = open(self.dir+"/"+wsname, 'rt', encoding='UTF8')
+                FH = open(self.dir+"/"+wsname, 'r', encoding=selected_encode)
             except IOError:
+                # print("[-] 에러: 텍스트 파일 오픈 에러, 파일이 존재하는지 확인해주세요.")
+                wb.close()
                 return 1
-                #print("[-] 에러: 텍스트 파일 오픈 에러, 파일이 존재하는지 확인해주세요.")
 
             # 워크시트 선택, 세부설정
             # ws = wb.active active는 첫번째 워크시트를 선택하는 코드임
             ws = wb.get_sheet_by_name(os.path.splitext(wsname)[0])
             ws.column_dimensions[stcol].width = self.width
 
-            # 정규표현식 셋팅
+            # 정규표현식 컴파일
             pstart = re.compile(self.start)
             pend = re.compile(self.end)
 
             #텍스트 내용 가져오기
             contentList = []        #파싱포인트 배열
             nextcell = int(strow)   #셀 행 구분
-            # 파일의 내용을 라인 단위로 리스트에 저장  Standard_START, Standard_END
+            # 파일의 내용을 라인 단위로 리스트에 저장
             findPoint = False
             try:
                 for line in FH:
@@ -60,8 +67,6 @@ class MyParsing:
                         findPoint = True
                     #시작 포인트 찾았을때, 엔드포인트 진행
                     if findPoint:
-                        #print("파싱 포인트를 찾았습니다.")
-                        #print("일치하는 라인: %s" %line)
                         contentList.append(line)
                          # End 문구를 찾았을 때, 리스트에 기록된 내용을 시트에 저장
                         if pend.search(line) != None:
@@ -69,45 +74,47 @@ class MyParsing:
                             try:
                                 contentList.pop(0)
                                 contentList.pop(-1)
-                                #마지막 개행 문자만 제거
-                                contentList[-1] = contentList[-1].rstrip('\r')
-                                contentList[-1]=contentList[-1].rstrip('\n')
                             except IndexError:
                                 #print("[-] 에러: 시작/끝 파싱포인트가 매칭되지 않았습니다. 문자열 또는 정규표현식을 다시 확인해주세요.")
+                                wb.close()
                                 FH.close()
                                 return 2
+                            # 마지막 개행 문자만 제거
+                            contentList[-1] = contentList[-1].rstrip('\r')
+                            contentList[-1] = contentList[-1].rstrip('\n')
 
                             # 셀에 데이터 기록
-
                             ws.row_dimensions[nextcell].height = self.height
                             ws[stcol + str(nextcell)].alignment = Alignment(vertical='top', wrap_text=True)
                             try:
                                 ws[stcol + str(nextcell)] = "".join(contentList)
-                            # badcharacters 에러 이슈가 있었음
                             except openpyxl.utils.exceptions.IllegalCharacterError:
-                                print("배드케릭터에러...해당 셀의 기록은 패스됩니다.")
+                                print("[셀 기록 불가] 한셀에 32700 문자를 넘어간 경우 또는 파싱문자열 시작-끝이 쌍으로 존재하지 않은 경우 발생(파싱불일치)")
                                 pass
-
+                                #return 3
                             # 리스트 초기화, 다음 엑셀 행 반복
                             contentList = []
                             findPoint = False
                             nextcell += 1
             except UnicodeError:
                 #print("[-] 에러: 텍스트 파일의 인코딩 상태를 확인해주세요.")
+                wb.close()
                 FH.close()
-                return 3
+                return 4
 
-        # 모든 시트 기록 후 자원 반납
-        wb.save(filename)
-        FH.close()
+            # for문 바깥처리, 엑셀 저장 후 자원 반납
+            wb.save(filename)
+            wb.close()
+            FH.close()
 
+    #워크시트가 존재하지 않을 경우 새로 생성
     def checkSheet(self, filename):
         wb = openpyxl.load_workbook(filename)
         # wb.get_sheet_names() 모든 워크시트 이름 가져오기
         for wsname in self.wsList:
             try:
-                ws = wb[wsname]         # 워크시트가 존재함
-            except KeyError:            # 존재하지 않을 경우 새로 생성
+                ws = wb[wsname]
+            except KeyError:
                 ws = wb.create_sheet()
                 ws.title = wsname
             finally:
@@ -116,12 +123,11 @@ class MyParsing:
                 except PermissionError:
                     return 1
 
+    # 현재 디렉토리 파일 목록 획득, 텍스트 파일만 추출하기
     def getTxt(self, dir):
-        # 현재 디렉토리 파일 목록 획득, 텍스트 파일만 추출하기
         self.dir = dir
         fileList = os.listdir(dir)
         for file in fileList:
             if file.find('.txt') > 0:
                 self.txtList.append(file)
-                # 확장자 제거
-                self.wsList.append(os.path.splitext(file)[0])
+                self.wsList.append(os.path.splitext(file)[0]) # 확장자 제거
